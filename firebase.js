e0c1d9cd5593563a8715d3a041b77776f23c2949
Rex-International <gabrielmichaelofficialtz@gmail.com>
2026-06-14 21:41:20 +0300

    Add Firebase initialization and configuration

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTTV7h23M7RE4c4f3xa0GWWFVibs1HcOk",
+  authDomain: "star-universal-trade-55d73.firebaseapp.com",
+  projectId: "star-universal-trade-55d73",
+  storageBucket: "star-universal-trade-55d73.firebasestorage.app",
+  messagingSenderId: "528787064169",
+  appId: "1:528787064169:web:f52d2b3cb0e8d36a1c29f8",
+  measurementId: "G-ELLWGJZYFD"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
